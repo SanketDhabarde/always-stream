727d66a10b926553041277cd293852404d87273e
SanketDhabarde <sanketdhabarde13@gmail.com>
2022-05-03 10:14:49 +0530

    [Refactor]: Removed unused code

--- src/pages/SingleVideo/SingleVideo.jsx
+++ src/pages/SingleVideo/SingleVideo.jsx
@@ -1,5 +1,4 @@
-import axios from "axios";
-import React, { useEffect } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
 import { Sidebar, Spinner } from "../../components";
 import { useFetch, useTitle } from "../../hooks";
